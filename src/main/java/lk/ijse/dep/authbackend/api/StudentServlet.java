f4e66588374acc3aad66cf295f0a44baae971934
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-18 02:39:09 +0530

    Intergrate CP with the project and change context.xml

--- src/main/java/lk/ijse/dep/authbackend/api/StudentServlet.java
+++ src/main/java/lk/ijse/dep/authbackend/api/StudentServlet.java
@@ -4,10 +4,27 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "StudentServlet", value = "/students", loadOnStartup = 0)
 public class StudentServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+
+            try {
+                InitialContext ctx = new InitialContext();
+                DataSource dataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/cp");
+                System.out.println(dataSource.getConnection());
+            } catch (NamingException | SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Do Get");
